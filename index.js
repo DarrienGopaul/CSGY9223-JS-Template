5c8083471818d80253708c13d158002a3c70af94
DarrienGopaul <dgopaul9217@gmail.com>
2023-12-14 19:30:58 -0500

    added Discord.js to packages

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-import * as hello from './src/hello.js';
+// Require the necessary discord.js classes
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+import 'dotenv';
 
-export default {
-  hello,
-};
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once).
+// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+// It makes some properties non-nullable.
+client.once(Events.ClientReady, readyClient => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(process.env.DISCORD_TOKEN);
